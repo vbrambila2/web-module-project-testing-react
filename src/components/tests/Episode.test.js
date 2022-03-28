c2918976583171ed0c83162a1522f6b3859f1a71
Victor Brambila <victorbrambila22@gmail.com>
2022-03-28 13:04:21 -0700

    initial testing done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,7 +3,10 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+test("renders without error", async () => { 
+    render(<Episode episode={{ name: 'name', image: 'image', summary: 'summary', seasons: 'seasons' }}/>)
+    await screen.findByText('minutes')
+});
 
 test("renders the summary test passed as prop", () => { });
 
